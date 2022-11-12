c5aa8f71804522bbfebb73da7080b1200a8fdeea
DEDSacram <bubles013@gmail.com>
2022-11-12 14:52:10 +0100

    comments

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,10 @@ import "./index.css"
 import Canvas from "./components/canvas";
 
 export const App: Component = () => {
+  //window size
   const [size, setSize] = createSignal({ width: document.body.clientWidth, height: document.body.clientHeight });
+
+  //is menu opened
   const [open, setOpen] = createSignal(false)
   // ciphers
   const [textEncryption, setTextEncryption] = createSignal('');
@@ -21,8 +24,10 @@ export const App: Component = () => {
   let ref
   let resizerright
   let dragheader
+  // retain height reference
+  let [heightbefore, setHeightBefore] = createSignal("fit-content")
 
-
+  // wait timer before window resize optimalization
   function debounce(fn: { apply: (arg0: any, arg1: IArguments) => void; }, ms: number) {
     let timer: any
     return (_: any) => {
@@ -33,37 +38,48 @@ export const App: Component = () => {
       }, ms)
     };
   }
+  //resize window
   function handleResize() {
     setSize({ width: document.documentElement.clientWidth, height: document.documentElement.clientHeight })
   }
+  // calling timeout before resizing
   window.addEventListener('resize', debounce(handleResize, 200))
+
+  //creating canvas back and overlay references
   let canvas!: { getContext: (arg0: string) => any; width: number; height: number; }
   let overlaycanvas!: { getContext: (arg0: string) => any; width: number; height: number; }
 
-  let [heightbefore,setHeightBefore] = createSignal("fit-content")
-  function checkOpen(){
-    if(!open()){
+  // On open do 
+  function checkOpen() {
+    if (!open()) {
       setHeightBefore(ref.offsetHeight)
     }
     setOpen(!open())
     moveUp()
   }
-  function moveUp(){
-    if(!open()){
-      if(ref.offsetTop+heightbefore() >= window.innerHeight){
+  // check out of bounds if is move to top
+  function moveUp() {
+    if (!open()) {
+      if (ref.offsetTop + heightbefore() >= window.innerHeight) {
         let addto = ref.offsetTop
-        ref.style.top = addto-(ref.offsetTop+heightbefore() - window.innerHeight) + "px"
+        ref.style.top = addto - (ref.offsetTop + heightbefore() - window.innerHeight) + "px"
       }
       ref.style.height = heightbefore() + "px"
     }
-    else{
+    else {
       ref.style.height = "fit-content"
     }
   }
+  // Mounting after dom was rendered
   onMount(() => {
-    makeResizableDiv(ref,resizerright)
+    // resize and drag function on menu
+    makeResizableDiv(ref, resizerright)
     dragElement(ref, dragheader);
+
+    //context of canvas n1
     const ctx = canvas.getContext("2d");
+
+    //rendering loop
     let frame = requestAnimationFrame(loop);
 
     function loop(t: number) {
@@ -89,20 +105,21 @@ export const App: Component = () => {
     }
 
     onCleanup(() => cancelAnimationFrame(frame));
+    //end
 
 
 
     //overlay
 
     const ctx2 = canvas.getContext("2d");
+    //render loop
     let frame2 = requestAnimationFrame(loop2);
-
     function loop2(t: number) {
       frame2 = requestAnimationFrame(loop2);
     }
 
     onCleanup(() => cancelAnimationFrame(frame2));
-
+    //end
   });
 
 
@@ -113,11 +130,6 @@ export const App: Component = () => {
         <Canvas ref={canvas} width={size().width} height={size().height} overlay={false} />
         <Canvas ref={overlaycanvas} width={size().width} height={size().height} overlay={true} />
       </Suspense>
-
-
-
-
-
 
       <div id="dragdiv" ref={ref} class="z-50 w-3/12 flex flex-col absolute" >
         <div class="flex box-border border-2 bg-slate-800 "><div class="flex-initial w-3/4" ref={dragheader} id="dragdivheader"><FiMoreHorizontal color="white" /></div>{open() ? <FiChevronDown onClick={checkOpen} color="white" class="custom-icon z-60 w-1/4 flex-initial" title="a11y" /> : <FiChevronUp onClick={checkOpen} color="white" class="custom-icon z-60 w-1/4 flex-initial" title="a11y" />}</div>
@@ -131,20 +143,15 @@ export const App: Component = () => {
               setTextEncryption(e.target.value);
             }}
           />
-
           <label for="encrypttextkey">Cipher Key</label>
           <input name="encrypttextkey" class="text-black bg-gray-50 border border-gray-30 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
             onInput={(e) => {
               setTextEncryptionKey(e.target.value);
             }}
           />
-
           <button type="submit" class=" bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-
           <FiArrowDownRight ref={resizerright} class="self-end resizer"></FiArrowDownRight>
-
         </div>
-
       </div>
     </div>
 
