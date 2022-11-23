a0ff51252e298006b8694a28ea4bad60689fb36a
DEDSacram <bubles013@gmail.com>
2022-11-23 02:02:07 +0100

    res

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { render } from "solid-js/web";
-import { onMount} from "solid-js";
+import { onMount } from "solid-js";
 import { Component } from "solid-js";
-import {Suspense, createSignal } from "solid-js";
+import { Suspense, createSignal } from "solid-js";
 import { DropdownCipher } from "./components/dropdown";
 import { createPlayfair } from "./lib/createPlayfair";
 import { createCaesar } from "./lib/createCaesar";
@@ -9,9 +9,22 @@ import { createCaesarWheel } from "./lib/createCaesarWheel";
 import "./index.css"
 import Canvas from "./components/canvas";
 import { Menu } from "./components/menu";
-import {FiChevronsLeft,FiChevronLeft,FiPause,FiChevronRight,FiChevronsRight} from "solid-icons/fi";
+import { FiChevronsLeft, FiChevronLeft, FiPause, FiChevronRight, FiChevronsRight } from "solid-icons/fi";
 
-
+class Postreq {
+  public Cipher: number;
+  public Text: string;
+  public Key: string | number;
+  constructor(cipher: number, text: string, key: string | number) {
+    this.Cipher = cipher
+    this.Text = text
+    this.Key = key
+  }
+}
+interface Postres {
+  TextBefore: string;
+  TextNow: string;
+}
 export const App: Component = () => {
   //window size
   const [size, setSize] = createSignal({ width: document.body.clientWidth, height: document.body.clientHeight });
@@ -21,11 +34,33 @@ export const App: Component = () => {
   let currentfunction
   let backctx;
   let frontctx;
-  let currentcipher;
+  let currentcipher : number;
   //reset
   let submit = false;
 
-  
+  let encrypt = false;
+
+  //rescaling animation
+
+  let ratio = 1
+
+  // response from
+  let backres : Postres;
+  const res = async (data: Postreq,encrypt : boolean) => {
+    let url = 'http://localhost:5294/decrypt'
+    if (encrypt) {
+      url = 'http://localhost:5294/encrypt'
+    }
+    return await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json())
+      .then((data) => {
+        backres = data})
+  }
 
   const refCallback = (el) => {
     currentcipher = el
@@ -61,7 +96,7 @@ export const App: Component = () => {
     }
   }
 
-  function onSubmit() {
+  async function onSubmit() {
     if (textEncryption() && textEncryptionKey()) {
       submit = true
       if (currentfunction && submit) {
@@ -70,6 +105,9 @@ export const App: Component = () => {
         currentfunction(textEncryptionKey())
         resetContext(backctx)
         resetContext(frontctx)
+        await res(new Postreq(currentcipher,textEncryption(),textEncryptionKey()),encrypt)
+        console.log(backres.TextNow)
+
       }
     }
     function resetContext(ctx) {
@@ -114,11 +152,11 @@ export const App: Component = () => {
       </Menu>
       <Menu title={"AniMenu"} itemid={"animenu"} minwidth={450} minheight={81}>
         <div >
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiChevronsLeft/></button>
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiChevronLeft/></button>
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiPause/></button>
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiChevronRight/></button>
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiChevronsRight/></button>
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiChevronsLeft /></button>
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiChevronLeft /></button>
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiPause /></button>
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiChevronRight /></button>
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><FiChevronsRight /></button>
         </div>
       </Menu>
     </div>
