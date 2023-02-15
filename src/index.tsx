95885bccd44f3fc1fcd99e3547192ccc54e88188
DEDSacram <bubles013@gmail.com>
2023-02-16 00:53:03 +0100

    colors

--- src/index.tsx
+++ src/index.tsx
@@ -396,7 +396,21 @@ export const App: Component = () => {
         let time = new Date().getTime() - start;
         ctx.clearRect(0, 0, overlaycanvas.width, overlaycanvas.height);
         let value =  easeInOutQuart(time, from, to - from, duration);
-        ctx.fillStyle = color;
+
+
+     
+        // ctx.fillStyle = color;
+        //Test
+        let percentage = (time/duration);
+     
+        ctx.fillStyle = `rgb(${(0.5-percentage)*255+128},${(percentage)*150},0)`;
+        // if(percentage < 0.5){
+        //   ctx.fillStyle = `rgb(${(percentage)*128},0, 0)`;
+        // }else{
+        //   ctx.fillStyle = `rgb(0,${(percentage)*128}, 0)`;
+        // }
+      
+
         if (row == undefined) {
           ctx.fillRect(value, col, width, height);
         } else {
